e74a4dcce22025f369a8a212fa6a83b58fe7cbe2
juan <juankrios15@gmail.com>
2023-07-03 12:42:15 -0500

    leaderboard design, footer design, anonymous user logic

--- test_mrv/static/js/base.js
+++ test_mrv/static/js/base.js
@@ -6,3 +6,13 @@ function toggleBold(checkbox) {
         label.classList.remove("fw-bold");
     }
 }
+
+function handleConfigClick(event) {
+    event.preventDefault();
+    var anonymousUser = localStorage.getItem('AnonymousUser');
+    if (anonymousUser === 'true') {
+        window.location.href = "/register";
+    } else {
+        window.location.href = event.target.href;
+    }
+}
